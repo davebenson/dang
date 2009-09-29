023a1b2d00d8291e5a9d0c201d539ac021ff11bf
lahiker42 <lahiker42@995540e8-a175-11de-ba4d-5dbaeb624ccf>
2009-09-29 01:29:54 +0000

    ...

--- trunk/dang-array.h
+++ trunk/dang-array.h
@@ -2,9 +2,25 @@
 typedef struct _DangArray DangArray;
 struct _DangArray
 {
-  DangTensor *tensor;
+  DangArray *tensor;
   unsigned ref_count;
   unsigned alloced;             /* allocated size of first dimension */
+};
+
+typedef struct _DangValueTypeArray DangValueTypeArray;
+struct _DangValueTypeArray
+{
+  DangValueType base_type;
+  DangValueType *element_type;
+  unsigned rank;
+
+  /* cached */
+  DangFunction *to_string_function;
+
+  DangValueTypeArray *left, *right, *parent;
+  dang_boolean is_red;
+
+  DangValueIndexInfo index_infos[2];
 };
 
 DangValueType *dang_value_type_array (DangValueType *element_type,
