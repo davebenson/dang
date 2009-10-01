edda535bd58bab8950207ffac4317b4141e98255
lahiker42 <lahiker42@995540e8-a175-11de-ba4d-5dbaeb624ccf>
2009-10-01 15:10:27 +0000

    ...

--- trunk/dang-array.c
+++ trunk/dang-array.c
@@ -199,6 +199,347 @@ index_set__array   (DangValueIndexInfo *ii,
   return TRUE;
 }
 
+/* ---- operator_assign_concat (<>=) --- */
+
+static DANG_SIMPLE_C_FUNC_DECLARE (do_vector_append_element)
+{
+  DangArray *array = * (DangArray **) args[0];
+  const void *value = args[1];
+  DangValueTypeArray *atype = func_data;
+  DangValueType *elt_type = atype->element_type;
+  DangVector *vec;
+  DANG_UNUSED (rv_out);
+  if (array == NULL)
+    {
+      dang_set_error (error, "null pointer exception (array while appending)");
+      return FALSE;
+    }
+  vec = (DangVector *) (array->tensor);
+  if (array->alloced == array->tensor->sizes[0]
+   || array->tensor->ref_count > 1)
+    {
+      unsigned new_len = array->alloced;
+      if (!DANG_IS_POWER_OF_TWO (new_len))
+        {
+          new_len = 2;
+          while (new_len <= vec->len)
+            new_len += new_len;
+        }
+      else
+        {
+          new_len *= 2;
+        }
+      if (vec->ref_count > 1)
+        {
+          /* must allocate a new tensor */
+          DangVector *new_vec = dang_new (DangVector, 1);
+          new_vec->ref_count = 1;
+          new_vec->len = vec->len;
+          new_vec->data = dang_malloc (elt_type->sizeof_instance * new_len);
+          dang_value_bulk_copy (atype->element_type, new_vec->data, vec->data, vec->len);
+          vec->ref_count--;
+          vec = new_vec;
+          array->tensor = (DangTensor *) new_vec;
+        }
+      else /* vec->alloced == vec->length -- must resize */
+        {
+          /* call realloc() */
+          vec->data = dang_realloc (vec->data, elt_type->sizeof_instance * new_len);
+        }
+      array->alloced = new_len;
+    }
+
+  /* copy the final element */
+  if (elt_type->init_assign)
+    elt_type->init_assign (elt_type,
+                           (char*)vec->data
+                           + vec->len * elt_type->sizeof_instance,
+                           value);
+  else
+    memcpy ((char*)vec->data + vec->len * elt_type->sizeof_instance,
+            value, elt_type->sizeof_instance);
+  ++(vec->len);
+  return TRUE;
+}
+static DANG_SIMPLE_C_FUNC_DECLARE (do_vector_append_vector)
+{
+  DangArray *array = * (DangArray **) args[0];
+  DangVector *src = * (DangVector **) args[1];
+  DangVector *dst;
+  DangValueTypeArray *atype = func_data;
+  DangValueType *elt_type = atype->element_type;
+  DANG_UNUSED (rv_out);
+  if (array == NULL)
+    {
+      dang_set_error (error, "null pointer exception (array while appending)");
+      return FALSE;
+    }
+  if (src == NULL)
+    return TRUE;
+  if (src->len + dst->len > array->alloced
+   || dst->ref_count > 1)
+    {
+      unsigned new_len = array->alloced;
+      unsigned min_len = src->len + dst->len;
+      if (!DANG_IS_POWER_OF_TWO (new_len))
+        new_len = 2;
+      while (new_len < min_len)
+        new_len += new_len;
+      if (dst->ref_count > 1)
+        {
+          /* must allocate a new tensor */
+          DangVector *new_dst = dang_new (DangVector, 1);
+          new_dst->ref_count = 1;
+          new_dst->len = dst->len;
+          new_dst->data = dang_malloc (elt_type->sizeof_instance * new_len);
+          dang_value_bulk_copy (atype->element_type, new_dst->data, dst->data, dst->len);
+          dst->ref_count--;
+          dst = new_dst;
+          array->tensor = (DangTensor *) new_dst;
+        }
+      else /* dst->alloced == dst->length -- must resize */
+        {
+          /* call realloc() */
+          dst->data = dang_realloc (dst->data, elt_type->sizeof_instance * new_len);
+        }
+      array->alloced = new_len;
+    }
+
+  dang_value_bulk_copy (atype->element_type,
+                        (char*)dst->data + elt_type->sizeof_instance * dst->len,
+                        src->data,
+                        src->len);
+  dst->len += src->len;
+  return TRUE;
+}
+
+
+static DANG_SIMPLE_C_FUNC_DECLARE (do_tensor_append_tensor)
+{
+  DangArray *array = * (DangArray **) args[0];
+  DangTensor *a;
+  DangTensor *b = * (DangTensor **) args[1];
+  DangValueTypeArray *atype = func_data;
+  DangValueType *elt_type = atype->element_type;
+  unsigned rank = atype->rank;
+  unsigned minor_count = 1;
+  unsigned i;
+  DANG_UNUSED (rv_out);
+  DANG_UNUSED (error);
+
+  if (array == NULL)
+    {
+      dang_set_error (error, "null pointer exception (tensor while appending)");
+      return FALSE;
+    }
+
+  /* XXX: technically should check that a is full of 0 length non-first sizes */
+  if (b == NULL)
+    return TRUE;
+
+  a = array->tensor;
+  if (a == NULL)
+    {
+      array->tensor = b;
+      b->ref_count++;
+      return TRUE;
+    }
+  for (i = 1; i < rank; i++)
+    {
+      if (a->sizes[i] != b->sizes[i])
+        {
+          dang_set_error (error, "size mismatch in dimension #%u to of <>= (size %u v %u)",
+                          i+1, a->sizes[i], b->sizes[i]);
+          return FALSE;
+        }
+      minor_count *= a->sizes[i];
+    }
+  if (b->sizes[0] == 0)
+    return TRUE;
+
+  if (a->sizes[0] + b->sizes[0] > array->alloced
+   || a->ref_count > 1)
+    {
+      unsigned new_len = array->alloced;
+      unsigned min_len = a->sizes[0] + b->sizes[0];
+      if (!DANG_IS_POWER_OF_TWO (new_len))
+        new_len = 2;
+      while (new_len < min_len)
+        new_len += new_len;
+      if (a->ref_count > 1)
+        {
+          /* must allocate a new tensor */
+          DangTensor *new_a = dang_malloc (DANG_TENSOR_SIZEOF (atype->rank));
+          new_a->ref_count = 1;
+          memcpy (new_a->sizes, a->sizes, sizeof (unsigned) * atype->rank);
+          new_a->data = dang_malloc (elt_type->sizeof_instance * minor_count * new_len);
+          dang_value_bulk_copy (elt_type, new_a->data, a->data, minor_count * a->sizes[0]);
+          a->ref_count--;
+          a = new_a;
+          array->tensor = new_a;
+        }
+      else /* must resize */
+        {
+          /* call realloc() */
+          a->data = dang_realloc (a->data, elt_type->sizeof_instance * minor_count * new_len);
+        }
+      array->alloced = new_len;
+    }
+
+  dang_value_bulk_copy (elt_type,
+                        ((char*)a->data) + (elt_type->sizeof_instance * minor_count * a->sizes[0]),
+                        b->data,
+                        minor_count * b->sizes[0]);
+  a->sizes[0] += b->sizes[0];
+  return TRUE;
+}
+
+static DANG_SIMPLE_C_FUNC_DECLARE (do_tensor_append_subtensor)
+{
+  DangArray *array = * (DangArray **) args[0];
+  DangTensor *a;
+  DangTensor *b = * (DangTensor **) args[1];
+  DangValueTypeArray *atype = func_data;
+  DangValueType *elt_type = atype->element_type;
+  unsigned rank = atype->rank;
+  unsigned minor_count = 1;
+  unsigned i;
+  void *start;
+  DANG_UNUSED (rv_out);
+  DANG_UNUSED (error);
+  if (array == NULL)
+    {
+      dang_set_error (error, "null pointer exception (tensor while appending)");
+      return FALSE;
+    }
+  a = array->tensor;
+  if (b == NULL)
+    return TRUE;
+  if (a == NULL)
+    {
+      array->tensor = b;
+      b->ref_count++;
+      return TRUE;
+    }
+
+  for (i = 1; i < rank; i++)
+    {
+      if (a->sizes[i] != b->sizes[i-1])
+        {
+          dang_set_error (error, "size mismatch in dimension #%u and #%u to of <>= (size %u v %u)",
+                          i+1, i, a->sizes[i], b->sizes[i-1]);
+          return FALSE;
+        }
+      minor_count *= a->sizes[i];
+    }
+
+  if (array->alloced == array->tensor->sizes[0]
+   || array->tensor->ref_count > 1)
+    {
+      unsigned new_len = array->alloced;
+      unsigned min_len = a->sizes[0] + 1;
+      if (!DANG_IS_POWER_OF_TWO (new_len))
+        new_len = 2;
+      while (new_len < min_len)
+        new_len += new_len;
+      if (a->ref_count > 1)
+        {
+          /* must allocate a new tensor */
+          DangTensor *new_a = dang_malloc (DANG_TENSOR_SIZEOF (rank));
+          new_a->ref_count = 1;
+          memcpy (new_a->sizes, a->sizes, sizeof (unsigned) * rank);
+          new_a->data = dang_malloc (elt_type->sizeof_instance * minor_count * new_len);
+          dang_value_bulk_copy (elt_type, new_a->data, a->data, minor_count * a->sizes[0]);
+          a->ref_count--;
+          a = new_a;
+          array->tensor = new_a;
+        }
+      else /* must resize */
+        {
+          /* call realloc() */
+          a->data = dang_realloc (a->data, elt_type->sizeof_instance * minor_count * new_len);
+        }
+      array->alloced = new_len;
+    }
+
+  start = (char*)a->data
+        + a->sizes[0] * minor_count * elt_type->sizeof_instance;
+  dang_value_bulk_copy (elt_type, start, b->data, minor_count * 1);
+  a->sizes[0] += 1;
+  return TRUE;
+}
+
+static DangFunction *
+try_sig__operator_assign_concat  (DangMatchQuery *query,
+                                  void *data,
+                                  DangError **error)
+{
+  DangValueType *type_b;
+  DangValueTypeArray *atype_a;
+  DangValueTypeTensor *ttype_b;
+  DangFunctionParam params[2];
+  DangSimpleCFunc simple_c;
+  DangSignature *sig;
+  DangFunction *rv;
+  DANG_UNUSED (data);
+  if (query->n_elements != 2
+   || query->elements[0].type != DANG_MATCH_QUERY_ELEMENT_SIMPLE_OUTPUT
+   || query->elements[1].type != DANG_MATCH_QUERY_ELEMENT_SIMPLE_INPUT
+   || !dang_value_type_is_array (query->elements[0].info.simple_output))
+    {
+      return NULL;
+    }
+  atype_a = (DangValueTypeArray*) query->elements[0].info.simple_output;
+  type_b = query->elements[1].info.simple_input;
+  if (atype_a->rank == 1
+   && atype_a->element_type == type_b)
+    {
+      simple_c = do_vector_append_element;
+      goto success;
+    }
+
+  if (!dang_value_type_is_tensor (type_b))
+    return NULL;
+  ttype_b = (DangValueTypeTensor*) type_b;
+  if (ttype_b->element_type != atype_a->element_type)
+    {
+      dang_set_error (error, "type mismatch in elements of tensor <>= : %s v %s",
+                      atype_a->element_type->full_name,
+                      ttype_b->element_type->full_name);
+      return NULL;
+    }
+  if (atype_a->tensor_type == type_b)
+    {
+      if (atype_a->rank == 1)
+        simple_c = do_vector_append_vector;
+      else
+        simple_c = do_tensor_append_tensor;
+    }
+  else if (atype_a->rank == ttype_b->rank + 1)
+    {
+      /* or higher rank equivalents */
+      simple_c = do_tensor_append_subtensor;
+    }
+  else
+    {
+      dang_set_error (error, "rank mismatch in tensor <>= : %s v %s",
+                      atype_a->base_type.full_name,
+                      ttype_b->base_type.full_name);
+      return NULL;
+    }
+
+success:
+  params[0].dir = DANG_FUNCTION_PARAM_INOUT;
+  params[0].type = (DangValueType *) atype_a;
+  params[0].name = NULL;
+  params[1].dir = DANG_FUNCTION_PARAM_IN;
+  params[1].type = type_b;
+  params[1].name = NULL;
+  sig = dang_signature_new (NULL, 2, params);
+  rv = dang_function_new_simple_c (sig, simple_c, atype_a, NULL);
+  dang_signature_unref (sig);
+  return rv;
+}
 static DANG_SIMPLE_C_FUNC_DECLARE (cast_from_array_to_tensor)
 {
   DangArray *array = * (DangArray **) args[0];
@@ -405,6 +746,14 @@ _dang_array_init (DangNamespace *the_ns)
     dang_die ("adding 'to_string' for array failed");
   dang_function_family_unref (family);
 
+  family = dang_function_family_new_variadic_c ("array_operator_assign_concat",
+                                                try_sig__operator_assign_concat,
+                                                NULL,
+                                                NULL);
+  if (!dang_namespace_add_function_family (the_ns, "operator_assign_concat",
+                                           family, &error))
+    dang_die ("adding 'to_string' for array failed");
+  dang_function_family_unref (family);
 
 }
 
