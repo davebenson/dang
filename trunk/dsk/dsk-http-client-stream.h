0bb2480f2accc528f388210b2d0587c9027bdfae
lahiker42 <lahiker42@995540e8-a175-11de-ba4d-5dbaeb624ccf>
2010-02-05 18:28:05 +0000

    ..

--- trunk/dsk/dsk-http-client-stream.h
+++ trunk/dsk/dsk-http-client-stream.h
@@ -1,18 +1,67 @@
+typedef struct _DskHttpClientStreamClass DskHttpClientStreamClass;
 typedef struct _DskHttpClientStream DskHttpClientStream;
+typedef struct _DskHttpClientStreamFuncs DskHttpClientStreamFuncs;
+typedef struct _DskHttpClientStreamTransfer DskHttpClientStreamTransfer;
+
+struct _DskHttpClientStreamClass
+{
+  DskObject base_instance;
+};
 struct _DskHttpClientStream
 {
+  DskObject base_instance;
   DskOctetSink *sink;
   DskOctetSource *source;
   DskHttpClientStreamTransfer *first_transfer, *last_transfer;
 };
 
-DskHttpClientStreamTransfer *
-dsk_http_client_stream_request (DskHttpClientStream *stream,
-                                DskHttpRequest      *request,
-				DskOctetSource      *post_data,
-				DskHttpRequestFunc   func,
-				DskHttpRequestErrorFunc error_func,
-				void                *func_data,
-				DskHookDestroy       destroy);
+/* internals */
+struct _DskHttpClientStreamTransfer
+{
+  DskHttpClientStream *owner;
+  DskHttpRequest *request;
+  DskHttpResponse *response;
+  DskMemoryBufferSource *content;      
+  DskHttpClientStreamTransfer *next;
+  DskHttpClientStreamFuncs *funcs;
+  void *user_data;
+};
 
+DskHttpClientStream *
+dsk_http_client_stream_new     (DskOctetSink        *sink,
+                                DskOctetSource      *source);
+
+typedef enum
+{
+  DSK_HTTP_CLIENT_STREAM_ERROR_PREMATURE_SHUTDOWN,
+  DSK_HTTP_CLIENT_STREAM_ERROR_BAD_HEADER,
+  DSK_HTTP_CLIENT_STREAM_ERROR_BAD_CONTENT,
+} DskHttpClientStreamError;
+
+struct _DskHttpClientStreamFuncs
+{
+  /* called once the http header is received.
+     content may or may not be complete. */
+  void (*handle_response)         (DskHttpClientStreamTransfer *transfer);
+
+  /* called once content is completely received */
+  void (*handle_content_complete) (DskHttpClientStreamTransfer *transfer);
+
+  /* call for any number of errors */
+  void (*handle_error)            (DskHttpClientStreamTransfer *transfer);
+
+  /* always called exactly once after all other functions done */
+  void (*destroy)                 (DskHttpClientStreamTransfer *transfer);
+};
+
+/* note that 'funcs' must exist for the duration of the request.
+ * usually this is done by having a static DskHttpClientStreamFuncs.
+ * You could also free 'funcs' in the 'destroy' method.
+ */
+DskHttpClientStreamTransfer *
+dsk_http_client_stream_request (DskHttpClientStream      *stream,
+                                DskHttpRequest           *request,
+				DskOctetSource           *post_data,
+				DskHttpClientStreamFuncs *funcs,
+				void                     *user_data);
 
--- trunk/dsk/dsk-octet-io.h
+++ trunk/dsk/dsk-octet-io.h
@@ -1,10 +1,12 @@
 
+typedef struct _DskOctetSourceClass DskOctetSourceClass;
+typedef struct _DskOctetSinkClass DskOctetSinkClass;
 typedef struct _DskOctetSource DskOctetSource;
+typedef struct _DskOctetSink DskOctetSink;
 
-struct _DskOctetSource
+struct _DskOctetSourceClass
 {
-  DskObject base_instance;
-  DskHook *readable_hook;
+  DskObjectClass base_class;
 
   /* returns -1 on error */
   int         (*read)        (DskOctetSource *source,
@@ -15,21 +17,37 @@ struct _DskOctetSource
                               DskBuffer      *read_buffer,
                               DskError      **error);
 
+  /* not always implemented */
+  void        (*shutdown)    (DskOctetSource *source);
 };
-typedef struct _DskOctetSink DskOctetSink;
 
-struct _DskOctetSink
+struct _DskOctetSource
 {
   DskObject base_instance;
-  DskHook *writable_hook;
+  DskHook *readable_hook;
+};
+
+struct _DskOctetSinkClass
+{
+  DskObjectClass base_class;
 
   /* returns -1 on error */
-  int         (*write)       (DskOctetSink   *sink,
-                              unsigned        max_len,
-                              const void     *data_out,
-                              DskError      **error);
+  int         (*write)        (DskOctetSink   *sink,
+                               unsigned        max_len,
+                               const void     *data_out,
+                               DskError      **error);
   int         (*write_buffer) (DskOctetSink   *sink,
                                DskBuffer      *write_buffer,
                                DskError      **error);
 
+  /* not always implemented */
+  void        (*shutdown)     (DskOctetSink   *sink);
 };
+struct _DskOctetSink
+{
+  DskObject base_instance;
+  DskHook *writable_hook;
+};
+
+extern DskOctetSourceClass dsk_octet_source_class;
+extern DskOctetSinkClass dsk_octet_sink_class;
