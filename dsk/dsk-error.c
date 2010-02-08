4b0c6c8265c57187406dfe5778fce00698462037
lahiker42 <lahiker42@995540e8-a175-11de-ba4d-5dbaeb624ccf>
2010-02-08 21:35:15 +0000

    ..

---
+++ dsk/dsk-error.c
@@ -0,0 +1,56 @@
+#include "dsk.h"
+
+static void dsk_error_finalize (DskError *error)
+{
+  dsk_free (error->message);
+}
+
+DskErrorClass dsk_error_class =
+{
+  DSK_OBJECT_CLASS_DEFINE(DskError, &dsk_object_class,
+                          NULL,
+                          dsk_error_finalize)
+};
+
+
+DskError *dsk_error_new        (const char *format,
+                                ...)
+{
+  va_list args;
+  DskError *rv;
+  va_start (args, format);
+  rv = dsk_error_new_valist (format, args);
+  va_end (args);
+  return rv;
+}
+
+DskError *dsk_error_new_valist (const char *format,
+                                va_list     args)
+{
+  char buf[512];
+  DskError *rv;
+  vsnprintf (buf, sizeof (buf), format, args);
+  buf[sizeof(buf) - 1] = 0;
+  rv = dsk_object_new (&dsk_error_class);
+  rv->message = dsk_strdup (buf);
+  return rv;
+}
+
+DskError *dsk_error_new_literal(const char *message)
+{
+  DskError *rv = dsk_object_new (&dsk_error_class);
+  rv->message = dsk_strdup (message);
+  return rv;
+}
+
+DskError *dsk_error_ref        (DskError   *error)
+{
+  dsk_assert (dsk_object_is_a (error, &dsk_error_class));
+  return dsk_object_ref (error);
+}
+
+void      dsk_error_unref      (DskError   *error)
+{
+  dsk_assert (dsk_object_is_a (error, &dsk_error_class));
+  dsk_object_unref (error);
+}
