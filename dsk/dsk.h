967183c8304a8f8da61e9f88a5f6a69877f84532
lahiker42 <lahiker42@995540e8-a175-11de-ba4d-5dbaeb624ccf>
2010-02-24 15:38:51 +0000

    misc

--- dsk/dsk.h
+++ dsk/dsk.h
@@ -5,6 +5,7 @@
 #include "dsk-util.h"
 #include "dsk-object.h"
 #include "dsk-octet-io.h"
+#include "dsk-dns-protocol.h"
 #include "dsk-dns-client.h"
 #include "dsk-client-stream.h"
 #include "dsk-http-client-stream.h"
