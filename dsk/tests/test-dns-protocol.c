728c35840db4c7c3811760fd2c007db0755a7b5e
lahiker42 <lahiker42@995540e8-a175-11de-ba4d-5dbaeb624ccf>
2010-02-25 17:06:03 +0000

    ..

--- dsk/tests/test-dns-protocol.c
+++ dsk/tests/test-dns-protocol.c
@@ -9,6 +9,8 @@ int main()
     static const char bindata[] = "\245\246\1\0\0\1\0\0\0\0\0\0\3www\3foo\3com\0\0\1\0\1";
     message = dsk_dns_message_parse (sizeof (bindata), (uint8_t*) bindata,
                                      &error);
+    if (error)
+      dsk_warning("error parsing message: %s", error->message);
     dsk_assert (message != NULL);
     dsk_free (message);
   }
