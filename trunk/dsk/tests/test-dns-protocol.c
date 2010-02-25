7720a0d97d888b4538037914c88164c9e6b099d3
lahiker42 <lahiker42@995540e8-a175-11de-ba4d-5dbaeb624ccf>
2010-02-25 15:34:03 +0000

    ..

--- trunk/dsk/tests/test-dns-protocol.c
+++ trunk/dsk/tests/test-dns-protocol.c
@@ -3,9 +3,10 @@
 int main()
 {
   DskDnsMessage *message;
+  DskError *error = NULL;
 
   {
-    static const char bindata[] = ""\245\246\1\0\0\1\0\0\0\0\0\0\3www\3foo\3com\0\0\1\0\1"";
+    static const char bindata[] = "\245\246\1\0\0\1\0\0\0\0\0\0\3www\3foo\3com\0\0\1\0\1";
     message = dsk_dns_message_parse (sizeof (bindata), (uint8_t*) bindata,
                                      &error);
     dsk_assert (message != NULL);
