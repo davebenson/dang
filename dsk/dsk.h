585c559dd53481e17a2edfd921b83c4e2ca04f98
lahiker42 <lahiker42@995540e8-a175-11de-ba4d-5dbaeb624ccf>
2010-02-27 05:11:37 +0000

    remove dsk-util

--- dsk/dsk.h
+++ dsk/dsk.h
@@ -10,13 +10,18 @@
 #include "dsk-buffer.h"
 #include "dsk-octet-io.h"
 #include "dsk-memory.h"
-#include "dsk-dns-protocol.h"
 #include "dsk-dns-client.h"
+
+#include "dsk-dns-protocol.h"
+
 #include "dsk-client-stream.h"
+
 #include "dsk-http-protocol.h"
 #include "dsk-http-client-stream.h"
 #include "dsk-http-client.h"
 #include "dsk-http-server-stream.h"
 #include "dsk-http-server.h"
+
+#include "dsk-cmdline.h"
 
 #undef _dsk_inline_assert
