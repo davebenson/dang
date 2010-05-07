4031b32719fb5759eb8fbdce1b0d133f6a5217f3
lahiker42 <lahiker42@995540e8-a175-11de-ba4d-5dbaeb624ccf>
2010-05-07 12:23:00 +0000

    ...

--- dsk/dsk-octet-listener.h
+++ dsk/dsk-octet-listener.h
@@ -25,3 +25,5 @@ DskIOResult dsk_octet_listener_accept (DskOctetListener        *listener,
                                        DskError               **error);
 
 extern const DskOctetListenerClass dsk_octet_listener_class;
+#define DSK_OCTET_LISTENER_GET_CLASS(object) DSK_OBJECT_CAST_GET_CLASS(DskOctetListener, object, &dsk_octet_listener_class)
+#define DSK_OCTET_LISTENER(object) DSK_OBJECT_CAST(DskOctetListener, object, &dsk_octet_listener_class)
