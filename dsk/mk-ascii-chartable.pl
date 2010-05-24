a1392d6860ecb37009cb0bc18783548287cac5c0
lahiker42 <lahiker42@995540e8-a175-11de-ba4d-5dbaeb624ccf>
2010-05-24 14:53:09 +0000

    ...

--- dsk/mk-ascii-chartable.pl
+++ dsk/mk-ascii-chartable.pl
@@ -12,5 +12,7 @@ for ('a'..'z', 'A'..'Z') { $table[ord($_)] |= 2 }
 for ('0'..'9') { $table[ord($_)] |= 4 }
 # set isxdigit bits
 for ('0'..'9', 'a'..'f', 'A'..'F') { $table[ord($_)] |= 8 }
+# set isalpha bits
+for ('a'..'z', 'A'..'Z') { $table[ord($_)] |= 2 }
 
 for (my $i = 0; $i < 256; $i++) { print sprintf("0x%02x,", $table[$i]); if ($i % 16 == 15) {print "\n"} }
