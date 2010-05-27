8f6a6ca2d81434a7ddb3cad9b56ad6d5fe79ba22
lahiker42 <lahiker42@995540e8-a175-11de-ba4d-5dbaeb624ccf>
2010-05-27 14:21:06 +0000

    ..

---
+++ dsk/dsk-http-server-stream.c
@@ -0,0 +1,146 @@
+typedef struct _DskHttpServerStreamClass DskHttpServerStreamClass;
+typedef struct _DskHttpServerStream DskHttpServerStream;
+typedef struct _DskHttpServerStreamFuncs DskHttpServerStreamFuncs;
+typedef struct _DskHttpServerStreamTransfer DskHttpServerStreamTransfer;
+
+struct _DskHttpServerStreamClass
+{
+  DskObject base_instance;
+};
+struct _DskHttpServerStream
+{
+  DskObject base_instance;
+  DskOctetSink *sink;
+  DskOctetSource *source;
+  DskBuffer incoming_data;
+  DskBuffer outcoming_data;
+  DskHttpServerStreamTransfer *first_transfer, *last_transfer;
+  DskHttpServerStreamTransfer *incoming_data_transfer, *outgoing_data_transfer;
+};
+
+/* internals */
+typedef enum
+{
+  DSK_HTTP_SERVER_STREAM_READ_NEED_HEADER,
+  DSK_HTTP_SERVER_STREAM_READ_IN_BODY,
+  DSK_HTTP_SERVER_STREAM_READ_IN_BODY_EOF,
+  DSK_HTTP_SERVER_STREAM_READ_IN_XFER_CHUNKED_HEADER,
+  DSK_HTTP_SERVER_STREAM_READ_IN_XFER_CHUNK,
+  DSK_HTTP_SERVER_STREAM_READ_DONE
+} DskHttpServerStreamReadState;
+struct _DskHttpServerStreamTransfer
+{
+  DskHttpServerStream *owner;
+  DskHttpRequest *request;
+  DskHttpResponse *response;
+  DskMemorySource *content;      
+  DskHttpServerStreamTransfer *next;
+  DskHttpServerStreamFuncs *funcs;
+  void *user_data;
+  DskHttpServerStreamReadState read_state;
+  /* branch of union depends on 'read_state' */
+  union {
+    /* number of bytes we've already checked for end of header. */
+    struct { unsigned checked; } need_header;
+
+    /* number of bytes remaining in content-length */
+    struct { uint64_t remaining; } in_body;
+
+    /* number of bytes remaining in current chunk */
+    struct { uint64_t remaining; } in_xfer_chunk;
+
+    /* no data for IN_XFER_CHUNKED_HEADER, DONE */
+  } read_info;
+};
+
+static dsk_boolean
+handle_source_readable (DskOctetSource *source,
+                        DskHttpServerStream *ss)
+{
+  switch (dsk_octet_source_read_buffer (source, &ss->incoming_data))
+    {
+    case DSK_IO_RESULT_SUCCESS:
+    case DSK_IO_RESULT_AGAIN:
+      return DSK_TRUE;
+    case DSK_IO_RESULT_EOF:
+      ...
+    case DSK_IO_RESULT_ERROR:
+      ...
+    }
+  if (ss->incoming_data.size == 0)
+    return DSK_TRUE;
+  if (ss->read_transfer == NULL)
+    {
+      /* new transfer */
+      DskHttpServerStreamTransfer *xfer = dsk_malloc (sizeof (DskHttpServerStreamTransfer));
+      xfer->owner = ss;
+      xfer->request = NULL;
+      xfer->response = NULL;
+      xfer->content = NULL;
+      xfer->read_state = DSK_HTTP_SERVER_STREAM_READ_NEED_HEADER;
+      xfer->read_info.need_header.checked = 0;
+
+      GSK_QUEUE_APPEND (GET_XFER_QUEUE (ss), xfer);
+      ss->read_transfer = xfer;
+    }
+restart_processing:
+  switch (ss->read_transfer->read_state)
+    {
+    case DSK_HTTP_SERVER_STREAM_READ_NEED_HEADER:
+      {
+        unsigned start = xfer->read_info.need_header.checked;
+        if (start == stream->incoming_data.size)
+          break;
+
+        if (!scan_for_end_of_http_header (&stream->incoming_data, 
+                                          &xfer->read_info.need_header.checked,
+                                          DSK_FALSE))
+          {
+            /* check for max header size */
+            if (xfer->read_info.need_header.checked > stream->max_header_size)
+              {
+                stream->read_trap = NULL;
+                client_stream_set_error (stream, xfer,
+                                         "header too long (at least %u bytes)",
+                                         (unsigned)xfer->read_info.need_header.checked);
+                do_shutdown (stream);
+                return DSK_FALSE;
+              }
+            goto return_true;
+          }
+      }
+    case DSK_HTTP_SERVER_STREAM_READ_IN_POST:
+      ...
+    case DSK_HTTP_SERVER_STREAM_READ_IN_POST_EOF:
+      ...
+    case DSK_HTTP_CLIENT_STREAM_READ_IN_XFER_CHUNKED_HEADER:
+      ...
+    case DSK_HTTP_CLIENT_STREAM_READ_IN_XFER_CHUNKED_HEADER_EXTENSION:
+      ...
+    case DSK_HTTP_CLIENT_STREAM_READ_IN_XFER_CHUNK:
+      ...
+    case DSK_HTTP_CLIENT_STREAM_READ_AFTER_XFER_CHUNK:
+      ...
+    case DSK_HTTP_CLIENT_STREAM_READ_AFTER_XFER_CHUNKED:
+      ...
+    case DSK_HTTP_CLIENT_STREAM_READ_XFER_CHUNK_TRAILER:
+      ...
+    case DSK_HTTP_CLIENT_STREAM_READ_XFER_CHUNK_FINAL_NEWLINE:
+      ...
+    case DSK_HTTP_SERVER_STREAM_READ_DONE:
+      dsk_assert_not_reached ();
+    }
+}
+
+DskHttpServerStream *
+dsk_http_server_stream_new     (DskOctetSink        *sink,
+                                DskOctetSource      *source)
+{
+  DskHttpServerStream *ss = dsk_object_new (&dsk_http_server_stream_class);
+  ss->sink = dsk_object_ref (sink);
+  ss->source = dsk_object_ref (source);
+  ss->read_trap = dsk_hook_trap (&source->readable_hook,
+                                 (DskHookFunc) handle_source_readable,
+                                 ss, NULL);
+  return ss;
+}
