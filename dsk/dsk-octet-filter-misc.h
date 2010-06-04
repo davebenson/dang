cf36b63e9e53b86fbdf5c05ca84c871575a6a531
lahiker42 <lahiker42@995540e8-a175-11de-ba4d-5dbaeb624ccf>
2010-06-04 12:04:28 +0000

    misc

--- dsk/dsk-octet-filter-misc.h
+++ dsk/dsk-octet-filter-misc.h
@@ -9,11 +9,13 @@ DskOctetFilter *dsk_url_encoder_new                (void);
 DskOctetFilter *dsk_url_decoder_new                (void);
 DskOctetFilter *dsk_c_quoter_new                   (void);
 DskOctetFilter *dsk_c_unquoter_new                 (void);
+DskOctetFilter *dsk_quote_printable_new            (void);
+DskOctetFilter *dsk_unquote_printable_new          (void);
 DskOctetFilter *dsk_utf8_to_utf16_converter_new    (dsk_boolean big_endian);
 DskOctetFilter *dsk_utf16_to_utf8_converter_new    (dsk_boolean initially_big_endian,
                                                     dsk_boolean require_endian_marker);
-DskOctetFilter *dsk_char_doubler_new               (char c);
-DskOctetFilter *dsk_char_undoubler_new             (char c,
+DskOctetFilter *dsk_byte_doubler_new               (char c);
+DskOctetFilter *dsk_byte_undoubler_new             (char c,
                                                     dsk_boolean ignore_errors);
 
 /* The "_take" suffix implies the reference-count is passed on all the filters,
