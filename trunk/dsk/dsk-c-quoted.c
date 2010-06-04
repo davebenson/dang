73589c428f6196308ffbf9e47a0159614293b1e3
lahiker42 <lahiker42@995540e8-a175-11de-ba4d-5dbaeb624ccf>
2010-06-04 04:39:53 +0000

    c-quoting

--- trunk/dsk/dsk-c-quoted.c
+++ trunk/dsk/dsk-c-quoted.c
@@ -58,7 +58,8 @@ dsk_octet_filter_c_quoter_process (DskOctetFilter *filter,
 unescaped_char_loop:
       {
         const uint8_t *in_start = in_data;
-        while (in_length > 0 && ' ' <= *in_data && *in_data <= 126)
+        while (in_length > 0 && ' ' <= *in_data && *in_data <= 126
+               && *in_data != '"' && *in_data != '\\')
           in_data++, in_length--;
         if (in_start < in_data)
           dsk_buffer_append (out, in_data - in_start, in_start);
@@ -69,6 +70,32 @@ unescaped_char_loop:
           }
         else
           {
+            /* handle single-character escapes */
+            switch (*in_data)
+              {
+#define WRITE_CASE_SIMPLE_ESCAPED(c, str) \
+            case c: \
+              dsk_buffer_append (out, 2, str);  \
+              in_data++; \
+              if (--in_length == 0) \
+                { \
+                  cquoter->last_char = UNESCAPED_CHAR; \
+                  return DSK_TRUE; \
+                } \
+              goto unescaped_char_loop
+#define WRITE_SINGLE_CHAR_ESCAPED_CASES \
+            WRITE_CASE_SIMPLE_ESCAPED ('\a', "\\a"); \
+            WRITE_CASE_SIMPLE_ESCAPED ('\b', "\\b"); \
+            WRITE_CASE_SIMPLE_ESCAPED ('\f', "\\f"); \
+            WRITE_CASE_SIMPLE_ESCAPED ('\n', "\\n"); \
+            WRITE_CASE_SIMPLE_ESCAPED ('\r', "\\r"); \
+            WRITE_CASE_SIMPLE_ESCAPED ('\t', "\\t"); \
+            WRITE_CASE_SIMPLE_ESCAPED ('\v', "\\v"); \
+            WRITE_CASE_SIMPLE_ESCAPED ('"' , "\\\""); \
+            WRITE_CASE_SIMPLE_ESCAPED ('\\', "\\\\");
+                WRITE_SINGLE_CHAR_ESCAPED_CASES
+              }
+
             last_char = *in_data++;
             in_length--;
             goto escaped_char_loop;
@@ -80,29 +107,38 @@ unescaped_char_loop:
 escaped_char_loop:
       while (in_length > 0)
         {
-          if (' ' < *in_data || *in_data > 126)
+          switch (*in_data)
             {
-              write_octal (out, last_char);
-              last_char = *in_data++;
-              in_length--;
-            }
-          else if ('0' <= *in_data && *in_data <= '9')
-            {
-              /* Write three-octal variant */
-              char buf[5] = { '\\',
-                              '0' + (*in_data>>6),
-                              '0' + ((*in_data>>3)&7),
-                              '0' + ((*in_data)&7),
-                              *in_data };
-              dsk_buffer_append (out, 5, buf);
-              in_data++;
-              in_length--;
-            }
-          else
-            {
-              write_octal (out, last_char);
-              last_char = UNESCAPED_CHAR;
-              goto unescaped_char_loop;
+            case '0': case '1': case '2': case '3': case '4':
+            case '5': case '6': case '7': case '8': case '9':
+              {
+                /* Write three-octal variant */
+                char buf[5] = { '\\',
+                                '0' + (*in_data>>6),
+                                '0' + ((*in_data>>3)&7),
+                                '0' + ((*in_data)&7),
+                                *in_data };
+                dsk_buffer_append (out, 5, buf);
+                in_data++;
+                in_length--;
+                goto unescaped_char_loop;
+              }
+            WRITE_SINGLE_CHAR_ESCAPED_CASES
+#undef WRITE_CASE_SIMPLE_ESCAPED
+#undef WRITE_SINGLE_CHAR_ESCAPED_CASES
+            default:
+              if (' ' < *in_data || *in_data > 126)
+                {
+                  write_octal (out, last_char);
+                  last_char = *in_data++;
+                  in_length--;
+                }
+              else
+                {
+                  write_octal (out, last_char);
+                  last_char = UNESCAPED_CHAR;
+                  goto unescaped_char_loop;
+                }
             }
         }
       cquoter->last_char = last_char;
@@ -116,6 +152,7 @@ dsk_octet_filter_c_quoter_finish  (DskOctetFilter *filter,
                                    DskError      **error)
 {
   DskOctetFilterCQuoter *cquoter = (DskOctetFilterCQuoter *) filter;
+  DSK_UNUSED (error);
   if (cquoter->last_char != UNESCAPED_CHAR)
     write_octal (out, cquoter->last_char);
   return DSK_TRUE;
@@ -126,5 +163,5 @@ DSK_OCTET_FILTER_SUBCLASS_DEFINE(static, DskOctetFilterCQuoter, dsk_octet_filter
 DskOctetFilter *
 dsk_c_quoter_new (void)
 {
-  return dsk_object_new (&dsk_octet_filter_class);
+  return dsk_object_new (&dsk_octet_filter_c_quoter_class);
 }
