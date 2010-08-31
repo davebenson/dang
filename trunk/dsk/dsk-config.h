1d673cbe03e3e1227229e2d6889332c1fca98929
lahiker42 <lahiker42@995540e8-a175-11de-ba4d-5dbaeb624ccf>
2010-08-31 14:29:06 +0000

    misc

--- trunk/dsk/dsk-config.h
+++ trunk/dsk/dsk-config.h
@@ -1,2 +1,6 @@
 
 #define DSK_ALIGNOF_INT        __alignof(int)
+#define DSK_ALIGNOF_FLOAT      __alignof(float)
+#define DSK_ALIGNOF_DOUBLE     __alignof(double)
+#define DSK_ALIGNOF_INT64      __alignof(int64_t)
+#define DSK_ALIGNOF_POINTER    __alignof(void*)
