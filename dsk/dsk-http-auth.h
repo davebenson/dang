068c3429046a6a8811a5647a9c806865b06c3976
lahiker42 <lahiker42@995540e8-a175-11de-ba4d-5dbaeb624ccf>
2010-10-04 16:38:45 +0000

    ..

---
+++ dsk/dsk-http-auth.h
@@ -0,0 +1,19 @@
+/* Low-level Authentification Handling,
+ * this should be independent of the other HTTP code.
+ */
+
+/* --- basic --- */
+/* NOTE: NUL is not included in the length,
+   and is not added by encode(). */
+unsigned dsk_http_auth_basic_get_length (const char *username,
+                                         const char *password);
+void     dsk_http_auth_basic_encode     (const char *username,
+                                         const char *password,
+                                         char       *encoded_data);
+
+
+/* --- digest --- */
+...
+
+/* --- oauth --- */
+...
