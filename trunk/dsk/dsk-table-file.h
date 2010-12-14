1d4b52502fccd56946ebb52f84704d449a17fac2
lahiker42 <lahiker42@995540e8-a175-11de-ba4d-5dbaeb624ccf>
2010-12-14 16:42:51 +0000

    ...

--- trunk/dsk/dsk-table-file.h
+++ trunk/dsk/dsk-table-file.h
@@ -55,34 +55,49 @@ void        dsk_table_file_reader_destroy     (DskTableFileReader *reader);
 DskTableFileSeeker *dsk_table_file_seeker_new (DskTableFileOptions *options,
                                                DskError           **error);
 
-/* Returns whether this value is in the set. */
-typedef dsk_boolean (*DskTableSeekerTestFunc) (unsigned               len,
-                                               const uint8_t         *data,
-                                               void                  *user_data);
+/* Returns whether this key is in the set. */
+typedef dsk_boolean (*DskTableSeekerTestFunc) (unsigned           key_len,
+                                               const uint8_t     *key_data,
+                                               void              *user_data);
 
 /* The comparison function should return TRUE if the value
-   is greater than or equal to some threshold determined by func_data. */
-dsk_boolean  dsk_table_file_seeker_find_first (DskTableFileSeeker    *seeker,
-					       DskTableSeekerTestFunc func,
-					       void                  *func_data,
-                                               DskError             **error);
-
-/* The comparison function should return TRUE if the value
-   is less than or equal to some threshold determined by func_data. */
-dsk_boolean  dsk_table_file_seeker_find_last  (DskTableFileSeeker    *seeker,
-					       DskTableSeekerTestFunc func,
-					       void                  *func_data,
-                                               DskError             **error);
-
-/* Information about our current location. */
-dsk_boolean  dsk_table_file_seeker_peek_cur   (DskTableFileSeeker    *seeker,
-					       unsigned              *len_out,
-					       const uint8_t        **data_out);
-dsk_boolean  dsk_table_file_seeker_peek_index (DskTableFileSeeker    *seeker,
-					       uint64_t              *index_out);
-
-/* Advance forward in the file. */
-dsk_boolean  dsk_table_file_seeker_advance    (DskTableFileSeeker    *seeker);
-
+   is greater than or equal to some threshold determined by func_data.
+   We return the first element for which the function returns TRUE.
+   This function returns FALSE if:
+     - no matching key is found (in which case *error will not be set)
+     - an error occurs (corrupt data or disk error)
+       (in which case *error will be set)
+ */
+dsk_boolean
+dsk_table_file_seeker_find       (DskTableFileSeeker    *seeker,
+                                  DskTableSeekerTestFunc func,
+                                  void                  *func_data,
+                                  unsigned              *key_len_out,
+                                  const void           **key_data_out,
+                                  unsigned              *value_len_out,
+                                  const void           **value_data_out,
+                                  DskError             **error);
+ 
+DskTableFileReader *
+dsk_table_file_seeker_find_reader(DskTableFileSeeker    *seeker,
+                                  DskTableSeekerTestFunc func,
+                                  void                  *func_data,
+                                  DskError             **error);
+ 
+dsk_boolean
+dsk_table_file_seeker_index      (DskTableFileSeeker    *seeker,
+                                  uint64_t               index,
+                                  unsigned              *key_len_out,
+                                  const void           **key_data_out,
+                                  unsigned              *value_len_out,
+                                  const void           **value_data_out,
+                                  DskError             **error);
+ 
+DskTableFileReader *
+dsk_table_file_seeker_index_reader(DskTableFileSeeker    *seeker,
+                                   uint64_t               index,
+                                   DskError             **error);
+ 
 
 void         dsk_table_file_seeker_destroy    (DskTableFileSeeker    *seeker);
+
