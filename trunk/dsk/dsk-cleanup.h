af401f94592e3f94bcbec63df0fa6c60e9b4c2e1
lahiker42 <lahiker42@995540e8-a175-11de-ba4d-5dbaeb624ccf>
2011-03-24 15:26:10 +0000

    Moved to git.

--- trunk/dsk/dsk-cleanup.h
+++
@@ -1,3 +0,0 @@
-
-/* can be used to free stuff before program termination */
-void dsk_cleanup (void);
--- trunk/dsk/dsk-xml-binding-internals.h
+++
@@ -1,7 +0,0 @@
-
-DskXmlBindingNamespace *
-           _dsk_xml_binding_parse_ns_str (DskXmlBinding *binding,
-                                          const char    *path,
-                                          const char    *ns_name,
-                                          const char    *contents,
-                                          DskError     **error);
